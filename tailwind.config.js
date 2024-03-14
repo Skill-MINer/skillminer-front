6b67ec207205eaba37b59a5ecca27c7b5150408f
Baptiste Prevost <baptiste.prevost@epfedu.fr>
2024-03-14 13:52:28 +0100

    Add new colors to theme configuration and add this colors into the navbar component

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,52 @@
 module.exports = {
   content: [
     './src/**/*.{html,ts}',
+    './src/**/**/*.{html,ts}',
     './node_modules/preline/preline.js',
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [
-    require('preline/plugin'),
-  ],
+    extend: {
+      colors: {
+        'primary': {
+          DEFAULT: "#F8F0D5",
+        },
+        'secondary': {
+          DEFAULT: "#DC9B34",
+          0: '#fefce8',
+          1: '#fef8c4',
+          2: '#feef8a',
+          3: '#fdde48',
+          4: '#faca18',
+          5: '#ecb008',
+          6: '#cc8704',
+          7: '#a46007',
+          8: '#874b0e',
+          9: '#723e12',
+        },
+        'accent': {
+          DEFAULT: '#A63A50',
+          0: '#fef1f5',
+          1: '#fee5ec',
+          2: '#fdcedd',
+          3: '#fba6c1',
+          4: '#f8719c',
+          5: '#f1437a',
+          6: '#df225e',
+          7: '#be154c',
+          8: '#9e1442',
+          9: '#86153d',
+        },
+        'background': {
+          DEFAULT: '#FFFBF8'
+        },
+        'textcolor': {
+          DEFAULT: '#2C1A1D'
+        
+        },
+      },
+    },
+    plugins: [
+      require('preline/plugin'),
+    ],
+  }
 }
-
