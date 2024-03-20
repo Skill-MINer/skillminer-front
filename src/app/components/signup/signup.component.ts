22a98787f2201228ea25f84789c94f936fe72673
Martin Prévost <martin.mprevost@gmail.com>
2024-03-20 12:21:30 +0100

    Update signup component with form validation

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,30 +1,41 @@
 import { Component } from '@angular/core';
+import { FormGroup, AbstractControl, Validators, ReactiveFormsModule, FormControl } from "@angular/forms";
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.sass'
 })
 export class SignupComponent {
-  firstName: string;
-  lastName: string;
-  email: string;
-  password: string;
+  protected readonly loginForm = new FormGroup({
+    firstName: new FormControl("", Validators.required),
+    lastName: new FormControl("", Validators.required),
+    email: new FormControl("", [Validators.required, Validators.email]),
+    password: new FormControl("", [Validators.required, Validators.pattern(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/)]),
+    confirmPassword: new FormControl("", Validators.required)
+  }, { validators: this.passwordsMatch });
 
-  constructor() {
-    this.firstName = '';
-    this.lastName = '';
-    this.email = '';
-    this.password = '';
+  passwordsMatch(control: AbstractControl) {
+    const group = <FormGroup>control;
+    const passwordControl = group.get('password');
+    const confirmPasswordControl = group.get('confirmPassword');
+  
+    if (passwordControl && confirmPasswordControl) {
+      const password = passwordControl.value;
+      const confirmPassword = confirmPasswordControl.value;
+  
+      return password === confirmPassword ? null : { notSame: true };
+    }
+  
+    return { notSame: true };
   }
 
   onSubmit() {
-    console.log('Form submitted');
-    console.log('First name: ' + this.firstName);
-    console.log('Last name: ' + this.lastName);
-    console.log('Email: ' + this.email);
-    console.log('Password: ' + this.password)
+    if (this.loginForm.valid) {
+      console.log("Form submitted!");
+      console.log(this.loginForm.value);
+    }
   }
 }
