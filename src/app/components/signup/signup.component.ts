62e5f4d18e7409ed1406c185a6b0f1e0e2226c22
Martin Prévost <martin.mprevost@gmail.com>
2024-03-20 11:06:34 +0100

    Add login and signup routes, update login and signup components

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -8,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrl: './signup.component.sass'
 })
 export class SignupComponent {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
 
+  constructor() {
+    this.firstName = '';
+    this.lastName = '';
+    this.email = '';
+    this.password = '';
+  }
+
+  onSubmit() {
+    console.log('Form submitted');
+    console.log('First name: ' + this.firstName);
+    console.log('Last name: ' + this.lastName);
+    console.log('Email: ' + this.email);
+    console.log('Password: ' + this.password)
+  }
 }
