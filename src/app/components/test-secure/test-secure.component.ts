6dada0a2915651628f3099d65a546638696f13bd
Baptiste Prevost <baptiste.prevost@epfedu.fr>
2024-03-21 12:35:09 +0100

    Add guard that checks if user is authenticated, if not redirect to login page

---
+++ src/app/components/test-secure/test-secure.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-secure',
+  standalone: true,
+  imports: [],
+  templateUrl: './test-secure.component.html',
+  styleUrl: './test-secure.component.sass'
+})
+export class TestSecureComponent {
+
+}
