32bd942d6393742f84448a478c81d7a89fc80c4b
Esscraye <esscrayebat@netcourrier.com>
2024-03-28 10:13:25 +0100

    feature(): add profile-update component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { RecoverAccountComponent } from './components/recover-account/recover-account.component';
+import { ProfileUpdateComponent } from './components/profile-update/profile-update.component';
 
 export const routes: Routes = [
   { path: '', component: WelcomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'recover-account', component: RecoverAccountComponent },
+  { path: 'profile-update', component: ProfileUpdateComponent },
 ];
