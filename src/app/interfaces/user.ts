cd7d085e7b550f24de64732e807133400c203dbd
Esscraye <esscrayebat@netcourrier.com>
2024-03-28 15:16:03 +0100

    feature(): front profile-update form

--- src/app/interfaces/user.ts
+++ src/app/interfaces/user.ts
@@ -2,7 +2,12 @@ export interface User {
     id?: string;
     nom?: string;
     prenom?: string;
+<<<<<<< HEAD
     email: string;
     password: string;
+=======
+    email?: string;
+    password?: string;
+>>>>>>> 68f8dbe (feature(): front profile-update form)
     description?: string;
 }
