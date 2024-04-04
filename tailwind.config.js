1d1d85d75ef20f57e1589740ef5eb6159a24e6a5
eliolg <elio.longuet@epfedu.fr>
2024-04-04 12:14:40 +0200

    feature-accueil(): Fix hover in home training search bar + hover property in tailwing config

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,17 @@ module.exports = {
       colors: {
         'secondary': {
           DEFAULT: "#F8F0D5",
+              0: "#fffaeb",
+              1: "#fef2ca",
+              2: "#fde390",
+              3: "#fcce56",
+              4: "#fbb72a",
+              5: "#f6960e",
+              6: "#dc7207",
+              7: "#b7500a",
+              8: "#943e0f",
+              9: "#793410",
+              hover: "#fde390",
         },
         'primary': {
           DEFAULT: "#DC9B34",
@@ -27,6 +38,7 @@ module.exports = {
           7: '#a46007',
           8: '#874b0e',
           9: '#723e12',
+          hover: '#ecb008',
         },
         'accent': {
           DEFAULT: '#A63A50',
@@ -40,6 +52,7 @@ module.exports = {
           7: '#be154c',
           8: '#9e1442',
           9: '#86153d',
+          hover: '#be154c',
         },
         'background': {
           DEFAULT: '#FFFBF8',
