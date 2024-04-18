06d8eef14035b625da6fd7570ef24abb9ef7c2b7
AlexisP <alexis.pouillieute@gmail.com>
2024-04-18 19:04:41 +0200

    moved the add bar into a component and did the show on click hide on mouse leave

---
+++ src/app/components/formation/buttons/add-title-or-text/add-title-or-text.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-title-or-text',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-title-or-text.component.html',
+  styleUrl: './add-title-or-text.component.sass',
+})
+export class AddTitleOrTextComponent {
+  isPlus_barVisible = true;
+  isAdd_title_or_text_buttonsVisible = false;
+
+  toggleVisibility(itemId: string) {
+    if (itemId === 'plus_bar') {
+      this.isPlus_barVisible = !this.isPlus_barVisible;
+      this.isAdd_title_or_text_buttonsVisible = !this.isPlus_barVisible; // Assurez-vous qu'un seul élément est visible à la fois
+    } else if (itemId === 'add-title-or-text_buttons') {
+      this.isAdd_title_or_text_buttonsVisible =
+        !this.isAdd_title_or_text_buttonsVisible;
+      this.isPlus_barVisible = !this.isAdd_title_or_text_buttonsVisible; // Assurez-vous qu'un seul élément est visible à la fois
+    }
+  }
+}
--- src/app/components/formation/create-sections-container/create-sections-container.component.ts
+++ src/app/components/formation/create-sections-container/create-sections-container.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { TextSectionComponent } from '../sections/text-section/text-section.component';
 import { Title1SectionComponent } from '../sections/title1-section/title1-section.component';
+import { AddTitleOrTextComponent } from '../buttons/add-title-or-text/add-title-or-text.component';
 
 @Component({
   selector: 'app-create-sections-container',
   standalone: true,
-  imports: [TextSectionComponent, Title1SectionComponent],
+  imports: [
+    TextSectionComponent,
+    Title1SectionComponent,
+    AddTitleOrTextComponent,
+  ],
   templateUrl: './create-sections-container.component.html',
   styleUrl: './create-sections-container.component.sass',
 })
