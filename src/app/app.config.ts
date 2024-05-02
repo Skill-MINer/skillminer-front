ea68f4f54e31b3501529aec4abc94d1be166675a
Martin Prévost <martin.mprevost@gmail.com>
2024-05-02 11:40:25 +0200

    pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { HttpClientModule, provideHttpClient, withInterceptors } from '@angular/common/http';
 
@@ -6,21 +6,24 @@ import { routes } from './app.routes';
 import { authInterceptor } from './interceptors/auth.interceptor';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
     providers: [
-        provideRouter(routes, withComponentInputBinding()),
-        provideHttpClient(
-            withInterceptors([
-                authInterceptor
-            ])
-        ),
-        provideAnimations(),
-        provideToastr({
-            timeOut: 3000,
-            positionClass: 'toast-bottom-right',
-            closeButton: true,
-            progressBar: true
-        }),
-    ]
+    provideRouter(routes, withComponentInputBinding()),
+    provideHttpClient(withInterceptors([
+        authInterceptor
+    ])),
+    provideAnimations(),
+    provideToastr({
+        timeOut: 3000,
+        positionClass: 'toast-bottom-right',
+        closeButton: true,
+        progressBar: true
+    }),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+]
 };
