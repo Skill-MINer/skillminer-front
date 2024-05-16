70e9ebdb0dc35dab384c7e5dbe1f4185d8ff371d
Esscraye <esscrayebat@netcourrier.com>
2024-05-16 10:42:31 +0200

    feature(): add the list of created formations on the profile

--- src/app/components/card-container/card-container.component.ts
+++ src/app/components/card-container/card-container.component.ts
@@ -1,22 +1,30 @@
-import { Component } from '@angular/core';
-import { FormationCardComponent } from '../formation-card/formation-card.component';
-import { CardContainerService } from '../../services/card-container.service';
-import { Formation } from '../../interfaces/formation';
+import { Input, Component } from '@angular/core';
+import { FormationCardComponent } from '@components/formation-card/formation-card.component';
+import { CardContainerService } from '@services/card-container.service';
+import { Formation } from '@interfaces/formation';
 @Component({
   selector: 'app-card-container',
   standalone: true,
   imports: [FormationCardComponent],
   templateUrl: './card-container.component.html',
-  styleUrl: './card-container.component.sass',
+  styleUrl: './card-container.component.sass'
 })
 export class CardContainerComponent {
+  @Input() status: 'full' | 'user' | string = 'full';
   constructor(private cardContainerService: CardContainerService) {}
   cards: Formation[] = [];
   getCards() {
     // get cards from server
-    this.cardContainerService
-      .getFormations()
-      .subscribe((cards) => (this.cards = cards));
+    if (this.status === 'user') {
+      this.cardContainerService
+        .getFormationsByUser()
+        .subscribe((cards) => (this.cards = cards));
+      return;
+    } else {
+      this.cardContainerService
+        .getFormations()
+        .subscribe((cards) => (this.cards = cards));
+    }
   }
   ngOnInit(): void {
     this.getCards();
