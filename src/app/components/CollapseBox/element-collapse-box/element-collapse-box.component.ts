66587d5dbb446448d7d6fabbbc39cf1feb101b99
Baptiste Prevost <baptiste.prevost@epfedu.fr>
2024-05-18 16:49:30 +0200

    refractor: Update CollapseBox component to use animations for toggling visibility

--- src/app/components/CollapseBox/element-collapse-box/element-collapse-box.component.ts
+++ src/app/components/CollapseBox/element-collapse-box/element-collapse-box.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, input, Input, Output } from '@angular/core';
 import { MarkdownEditorComponent } from '@app/components/markdown-editor/markdown-editor.component';
 import { Markdown } from '@app/interfaces/markdown';
+import {animate, state, style, transition, trigger} from '@angular/animations';
+
 
 @Component({
   selector: 'app-element-collapse-box',
@@ -8,6 +10,18 @@ import { Markdown } from '@app/interfaces/markdown';
   imports: [MarkdownEditorComponent],
   templateUrl: './element-collapse-box.component.html',
   styleUrl: './element-collapse-box.component.sass',
+  animations: [trigger('toggle', [
+    state('collapsed', style({opacity: 0.0, height: 0})),
+    state('expanded', style({opacity: 1.0, height: '*'})),
+    transition('collapsed => expanded', [
+      style({opacity: 0.0, height: 0}),
+      animate('0.3s ease-in-out', style({opacity: 1.0, height: '*'}))
+    ]),
+    transition('expanded => collapsed', [
+      style({opacity: 1.0, height: '*'}),
+      animate('0.3s ease-in-out', style({opacity: 0.0, height: 0}))
+    ])
+  ])]
 })
 export class ElementCollapseBoxComponent {
   [x: string]: any;
@@ -16,11 +30,16 @@ export class ElementCollapseBoxComponent {
   @Output() titleHasChanged = new EventEmitter<string>();
   @Output() markdownHasChanged = new EventEmitter<Markdown>();
 
+  public state: 'collapsed' | 'expanded' = 'expanded';
   isCollapseBoxVisible = false;
+
   constructor() {}
+
   toggleVisibility() {
-    this.isCollapseBoxVisible = !this.isCollapseBoxVisible;
+    //this.isCollapseBoxVisible = !this.isCollapseBoxVisible;
+    this.state = this.state === 'collapsed' ? 'expanded' : 'collapsed';
   }
+
   changeTitleToText() {
     const changeTitleElement = document.getElementById(
       'TitleChange'
