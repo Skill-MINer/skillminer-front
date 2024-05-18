26bda3332db851a41c1f724afc1d65ae742623e7
lflament <luc.flament@epfedu.fr>
2024-05-18 11:38:40 +0200

    First CollapseBox is modifiable, others follow from the first one.

--- src/app/components/CollapseBox/element-collapse-box/element-collapse-box.component.ts
+++ src/app/components/CollapseBox/element-collapse-box/element-collapse-box.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, input, Input, Output } from '@angular/core';
 import { MarkdownEditorComponent } from '@app/components/markdown-editor/markdown-editor.component';
 import { Markdown } from '@app/interfaces/markdown';
+
 @Component({
   selector: 'app-element-collapse-box',
   standalone: true,
@@ -9,13 +10,27 @@ import { Markdown } from '@app/interfaces/markdown';
   styleUrl: './element-collapse-box.component.sass',
 })
 export class ElementCollapseBoxComponent {
+  [x: string]: any;
+  @Input() title: string = 'Title ';
   @Input() markdown: Markdown = { text: '' };
+  @Output() titleHasChanged = new EventEmitter<string>();
   @Output() markdownHasChanged = new EventEmitter<Markdown>();
 
   isCollapseBoxVisible = false;
   constructor() {}
   toggleVisibility() {
     this.isCollapseBoxVisible = !this.isCollapseBoxVisible;
+  }
+  changeTitleToText() {
+    const changeTitleElement = document.getElementById(
+      'TitleChange'
+    ) as HTMLInputElement;
+    const titleElement = document.getElementById('TitleButton') as HTMLElement;
+
+    if (changeTitleElement && titleElement) {
+      this.title = changeTitleElement.value;
+      this.titleHasChanged.emit(this.title);
+    }
   }
   sendEventMarkdown(markdown: Markdown) {
     this.markdownHasChanged.emit(markdown);
