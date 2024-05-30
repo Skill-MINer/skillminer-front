4ae492cbfd38cbd78358f7b80edc56a5bbecf440
Baptiste Prevost <baptiste.prevost@epfedu.fr>
2024-05-30 11:20:28 +0200

    feat: Add toggleTitleVisibility event emitter to Title1SectionComponent

--- src/app/components/formation/sections/title1-section/title1-section.component.ts
+++ src/app/components/formation/sections/title1-section/title1-section.component.ts
@@ -12,6 +12,7 @@ export class Title1SectionComponent {
   @Input() title: string = 'defaut title';
   @Output() titleHasChanged = new EventEmitter<string>();
   @Output() titleHasFinishedToChange = new EventEmitter<string>();
+  @Output() toggleTitleVisibility = new EventEmitter<null>();
 
   constructor() { }
 
@@ -23,5 +24,6 @@ export class Title1SectionComponent {
   sendEventTitleEditFinish() {
     console.log('finish', this.title);
     this.titleHasFinishedToChange.emit(this.title);
+    this.toggleTitleVisibility.emit();
   }
 }
